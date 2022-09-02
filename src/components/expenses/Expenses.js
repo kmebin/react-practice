3fbed52e0e41a15ce8bb58d8f797424453e9ddba
kmebin <blowhuiee@gmail.com>
2022-09-02 17:40:28 +0900

    [ADD] Expenses 컴포넌트

---
+++ src/components/expenses/Expenses.js
@@ -0,0 +1,15 @@
+import Card from "../common/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  return (
+    <Card className='expenses'>
+      {props.expenses.map((expense) => (
+        <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
