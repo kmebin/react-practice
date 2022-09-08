fe8ea74969b81a892a9f7627990067a6222819e8
kmebin <blowhuiee@gmail.com>
2022-09-09 00:15:10 +0900

    [UPDATE] Expenses 리스트 컴포넌트 분리

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Card from "../common/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -12,19 +12,11 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-    ));
-  }
-
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter filteredYear={filteredYear} onChangeFilter={filterChangeHandler} />
-        {expensesContent}
+        <ExpensesList expenses={filteredExpenses}></ExpensesList>
       </Card>
     </div>
   );
