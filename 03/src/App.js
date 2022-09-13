76689c555174ef86833a5acc6d2d5decfba19411
kmebin <blowhuiee@gmail.com>
2022-09-13 14:17:33 +0900

    [INIT] 프로젝트 세팅

--- 03/src/App.js
+++ 03/src/App.js
@@ -1,37 +1,5 @@
-import { useState } from "react";
-
 const App = () => {
-  const [toDo, setToDo] = useState("");
-  const [toDoList, setToDoList] = useState([]);
-
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-
-    if (toDo.length === 0) {
-      return;
-    }
-    setToDoList((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-
-  return (
-    <div>
-      <h1>My To Do List ({toDoList.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type='text' placeholder='Write yoru to do...' />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDoList.map((toDo, index) => (
-          <li key={index}>{toDo}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
