7011c5996adbe5f4abab678daa4bd9e85a7fcb0f
kmebin <blowhuiee@gmail.com>
2022-09-14 22:15:57 +0900

    [UPDATE] Portal 적용해서 모달 렌더링 위치 수정

--- 03-users-list/src/components/common/ErrorModal.js
+++ 03-users-list/src/components/common/ErrorModal.js
@@ -1,22 +1,36 @@
+import ReactDOM from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
 import styles from "./ErrorModal.module.css";
 
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onClick}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.content}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onClick}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
     <>
-      <div className={styles.backdrop} onClick={props.onClick}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.content}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onClick}>Okay</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(<Backdrop onClick={props.onClick} />, document.getElementById("backdrop-root"))}
+      {ReactDOM.createPortal(
+        <ModalOverlay title={props.title} content={props.content} onClick={props.onClick} />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
